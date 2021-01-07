be47691ac785f13432405957fa345adba879c82f
janellebaguley <nellie.baguley@gmail.com>
2021-01-07 15:05:40 -0700

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      dropdownView: false
+    }
+  }
+  toggleDropdown = () => {
+    this.setState({dropdownView: !this.state.dropdownView})
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+    <header className='main-header'>
+    {/* <img src = 'https://startbootstrap.github.io/startbootstrap-agency/assets/img/header-bg.jpg'/> */}
+    <img src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg" alt="Start Bootstrap" />
+      <nav className = 'desktop-menu'>
+        <span>SERVICES</span>
+        <span>PORTFOLIO</span>
+        <span>ABOUT</span>
+        <span>TEAM</span>
+        <span>CONTACT</span>
+      </nav>
+    <div className='dropdown' onClick={this.toggleDropdown}>Menu</div>
+          {this.state.dropdownView
+          ? (
+            <nav className = 'mobile-menu'>
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </nav>
+          )
+          : null}
+    </header>
+    <body>
+      <section>
+        <h3>Welcome to Our Studio!</h3>
+        <h1>IT'S NICE TO MEET YOU</h1>
+        <button>TELL ME MORE</button>
+        
+      </section>
+      
+    </body>
     </div>
   );
+  }
 }
 
 export default App;
